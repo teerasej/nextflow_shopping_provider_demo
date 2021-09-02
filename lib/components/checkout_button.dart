86495ca48e22b1426cac0098113cbdf81b0ff024
Teerasej <teerasej@gmail.com>
2021-09-02 21:00:49 +0700

    กดเปิดไปหน้า check out

--- lib/components/checkout_button.dart
+++ lib/components/checkout_button.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:badges/badges.dart';
+import 'package:nextflow_shopping_provider_demo/check_out_page.dart';
 
 class CheckoutButton extends StatefulWidget {
   const CheckoutButton({Key? key}) : super(key: key);
@@ -22,7 +23,16 @@ class _CheckoutButtonState extends State<CheckoutButton> {
       ),
       child: IconButton(
         icon: Icon(Icons.shopping_cart_outlined),
-        onPressed: () {},
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) {
+                return CheckoutPage();
+              },
+            ),
+          );
+        },
       ),
     );
   }
