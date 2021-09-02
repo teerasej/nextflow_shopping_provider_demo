cf6c6b2c6499f87395c65989ce2c5800601e97e8
Teerasej <teerasej@gmail.com>
2021-09-02 17:53:36 +0700

    disable debug mode

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Nextflow Shopping',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.orange,
       ),
