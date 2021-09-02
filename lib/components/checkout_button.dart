c5bca0eca40a9904b2edddb1fd25784aa813077a
Teerasej <teerasej@gmail.com>
2021-09-02 20:40:05 +0700

    Show product's number in cart - Access product in cart via context

--- lib/components/checkout_button.dart
+++ lib/components/checkout_button.dart
@@ -1,23 +1,18 @@
 import 'package:flutter/material.dart';
 
 import 'package:badges/badges.dart';
+import 'package:nextflow_shopping_provider_demo/controllers/cart_notifier.dart';
+import 'package:provider/provider.dart';
 
-class CheckoutButton extends StatefulWidget {
+class CheckoutButton extends StatelessWidget {
   const CheckoutButton({Key? key}) : super(key: key);
-
-  @override
-  _CheckoutButtonState createState() => _CheckoutButtonState();
-}
-
-class _CheckoutButtonState extends State<CheckoutButton> {
-  int _itemCount = 3;
 
   @override
   Widget build(BuildContext context) {
     return Badge(
       position: BadgePosition.topEnd(top: 5, end: 5),
       badgeContent: Text(
-        '$_itemCount',
+        '${context.watch<CartNotifier>().shoppingCart.length}',
         style: TextStyle(color: Colors.white, fontSize: 12),
       ),
       child: IconButton(
