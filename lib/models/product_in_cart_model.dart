e311d0494c8fcbea79685e9412537c8de979905a
Teerasej <teerasej@gmail.com>
2021-09-02 21:24:59 +0700

    ปรับค่าเริ่มต้นเป็น 1

--- lib/models/product_in_cart_model.dart
+++ lib/models/product_in_cart_model.dart
@@ -1,7 +1,7 @@
 import 'package:nextflow_shopping_provider_demo/models/product_model.dart';
 
 class ProductInCartModel {
-  int count = 0;
+  int count = 1;
   ProductModel product;
 
   ProductInCartModel(this.product);
